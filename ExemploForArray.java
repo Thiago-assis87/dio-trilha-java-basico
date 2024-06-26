0850681497cf81f6139ff5a61a01ee5e44d2f9e8
Thiago Rodrigues <162535804+Thiago-assis87@users.noreply.github.com>
2024-06-26 01:40:50 -0300

    Add files via upload

---
+++ ExemploForArray.java
@@ -0,0 +1,12 @@
+public class ExemploForArray {
+    public static void main(String[] args) {
+
+        //em arrays o indice inicia em Zero
+        String alunos[] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
+
+       for (String aluno : alunos){
+        System.out.println("Nome do aluno é: " + aluno);
+        
+       }
+    }
+}
---
+++ SistemaMedida.java
@@ -0,0 +1,7 @@
+public class SistemaMedida {
+
+    public static void main(String[] args) {
+        
+    }
+}
+
